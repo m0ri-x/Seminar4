cb71e15107511bbda46f2f6714ff68d7f959ae00
Elena <elenaburluckaja@gmail.com>
2022-08-23 09:36:08 +0300

    Добавила решение задачи 29

---
+++ task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+
+int[] arr = new int [8];
+Console.WriteLine("введите восемь значений: ");
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Массив: ");
+Console.Write(string.Join("/", arr)); 
